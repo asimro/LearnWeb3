473684b2293d8e7eee83a444a96a1757cf6fc2e9
Asim Raza <asimro@hotmail.com>
2021-10-07 22:08:58 +0400

    071021

--- 2-web3_sendValueTransaction.js
+++ 2-web3_sendValueTransaction.js
@@ -1,39 +1,49 @@
 const Web3 = require("web3");
 const rpcURL = "https://ropsten.infura.io/v3/80c615a196014e1ca02ebafef471988d";
 const Tx = require("ethereumjs-tx").Transaction;
+require('dotenv').config()
 
 
 const web3 = new Web3(rpcURL);
 
 
 const accountS = "0x53Acdd0B5C83B9BA58849f07B8B88301C37f9619";
-const Private_Key = 'c85927cd1d3aedcfc615155149aaa8c13ca26f9d77704ed71abfc9e15a9949ef';
-const Private_KeyS = Buffer.from(Private_Key, 'hex');
+const private_key = process.env.pvtkey;
+const Private_KeyS = Buffer.from(private_key, 'hex');
 
 const accountR = "0x653A48939074AD27afBd28560642bBeCf9d8E1d7";
 
+const sendEth = async () => {
+    try {
+        const txCount = await web3.eth.getTransactionCount(accountS);
+        console.log("Nonce value:", txCount);
 
-web3.eth.getTransactionCount(accountS, (err, txCount) => {
-    console.log("Nonce value:", txCount);
+        const txObject = {
+            nonce: web3.utils.toHex(txCount),
+            to: accountR,
+            value: web3.utils.toHex(web3.utils.toWei('0.1', 'ether')),
+            gasLimit: web3.utils.toHex(21000),
+            gasPrice: web3.utils.toHex(web3.utils.toWei('10', 'gwei'))
+        }
 
-    const txObject = {
-        nonce: web3.utils.toHex(txCount),
-        to: accountR,
-        value: web3.utils.toHex(web3.utils.toWei('0.1', 'ether')),
-        gasLimit: web3.utils.toHex(21000),
-        gasPrice: web3.utils.toHex(web3.utils.toWei('10', 'gwei'))
+        const tx = new Tx(txObject, { 'chain': 'ropsten' });
+        tx.sign(Private_KeyS);
+        const serialize = tx.serialize();
+        const raw = '0x' + serialize.toString('hex');
+
+        const txHash = await web3.eth.sendSignedTransaction(raw);
+        console.log('Transaction Hash', txHash);
+        // if (!err) {
+        //     console.log('Transaction Seccessful:', txHash);
+        // } else {
+        //     console.log('Transaction Error:', error);
+        // }
     }
 
-    const tx = new Tx(txObject, { 'chain': 'ropsten' });
-    tx.sign(Private_KeyS);
-    const serialize = tx.serialize();
-    const txHex = '0x' + serialize.toString('hex');
+    catch (e) {
+        console.log('Error', e);
+    }
 
-    web3.eth.sendSignedTransaction(txHex, (err, txHash) => {
-        if (!err) {
-            console.log('Transaction Seccessful:', txHash);
-        } else {
-            console.log('Transaction Error:', error);
-        }
-    });
-})
+}
+
+sendEth();
